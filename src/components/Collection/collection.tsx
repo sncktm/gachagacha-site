df069acc1f11e47acb9ed1ce424b7bf7fad02e43
sncktm <sncktm01@gmail.com>
2024-09-29 17:57:44 +0900

    COLLECTION修正

--- src/components/Collection/collection.tsx
+++ src/components/Collection/collection.tsx
@@ -3,7 +3,7 @@ import styles from "./collection.module.css";
 
 export default function Collection() {
   type Collection = {
-    id: string;
+    id: number;
     title: string;
     text: string;
     image: string;
@@ -11,19 +11,19 @@ export default function Collection() {
 
   const collections: Collection[] = [
     {
-      id: "1",
+      id: 1,
       title: "見返して楽しい",
       text: "ガチャガチャに必ずついてくる説明書は、一枚ずつファイリングしています。",
       image: "/images/collection1.png",
     },
     {
-      id: "2",
+      id: 2,
       title: "コンパクトに収納",
       text: "鞄につけたり、飾ったりしないガチャガチャは、小分けにして収納しています。",
       image: "/images/collection2.png",
     },
     {
-      id: "3",
+      id: 3,
       title: "収納だけじゃない",
       text: "お気に入りのフィギュアは、重ねられるケースに入れて飾っています。",
       image: "/images/collection3.png",
@@ -31,36 +31,37 @@ export default function Collection() {
   ];
 
   return (
-    <section id="sec3">
-      <h3>COLLECTION</h3>
-      <div className="collection">
-        <dl>
+    <>
+      <div className={styles.nami} />
+      <section className={styles.sec3}>
+        <h3>COLLECTION</h3>
+        <div className={styles.collections}>
           {collections.map((collection) => (
-            <div key={collection.id}>
-              <dt>
-                <a
-                  href={"#acc" + collection.id}
-                  className={collection.id === "1" ? "currentBtn" : ""}
-                >
-                  {collection.id}
-                </a>
-              </dt>
-              <dd id={"acc" + collection.id} className="current3">
-                <Image
-                  src={collection.image}
-                  width={500}
-                  height={400}
-                  alt={collection.title}
-                />
-                <div className="text">
-                  <h4>{collection.title}</h4>
-                  <p>{collection.text}</p>
-                </div>
-              </dd>
+            <div
+              key={collection.id}
+              className={`${styles.collection} ${
+                collection.id % 2 === 0 ? styles.reverse : ""
+              }`}
+            >
+              <Image
+                className={styles.image}
+                src={collection.image}
+                width={500}
+                height={300}
+                alt={collection.title}
+              />
+              <div
+                className={`${styles.text} ${
+                  collection.id % 2 ? styles.textLeft : styles.textRight
+                }`}
+              >
+                <h4>{collection.title}</h4>
+                <p>{collection.text}</p>
+              </div>
             </div>
           ))}
-        </dl>
-      </div>
-    </section>
+        </div>
+      </section>
+    </>
   );
 }
