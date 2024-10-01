f7c5ce67284d7e3f05b178d7373d073aedd57d55
sncktm <sncktm01@gmail.com>
2024-10-02 02:26:54 +0900

    hero修正

--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -14,8 +14,6 @@ export default function Hero({ title }: Props) {
         width={800} // 幅を小さく指定
         height={200} // 高さを小さく指定
         alt={title}
-        layout="intrinsic" // 画像のサイズを明示的に指定
-        objectFit="cover" // 画像の表示を親要素に合わせて調整
       />
       <h2 className={styles.h}>{title}</h2>
     </div>
