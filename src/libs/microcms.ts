cf4a6de08e045e2066e16d3d904c0ee11cd7d273
sncktm <sncktm01@gmail.com>
2024-10-01 23:48:45 +0900

    microCMS関連の修正

--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -12,23 +12,3 @@ export type Item = {
   price: number;
   releaseDate: string;
 } & MicroCMSListContent;
-
-if (!process.env.MICROCMS_SERVICE_DOMAIN) {
-  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
-}
-if (!process.env.MICROCMS_API_KEY) {
-  throw new Error("MICROCMS_API_KEY is required");
-}
-
-const client = createClient({
-  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
-  apiKey: process.env.MICROCMS_API_KEY,
-});
-
-export const getItemList = async (queries?: MicroCMSQueries) => {
-  const listData = await client.getList<Item>({
-    endpoint: "items",
-    queries,
-  });
-  return listData;
-};
--- src/pages/api/getContent.ts
+++ src/pages/api/getContent.ts
@@ -11,19 +11,17 @@ const client = createClient({
   serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN,
   apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
 });
-// APIハンドラーの作成
+
 export default async function GET(req: NextApiRequest, res: NextApiResponse) {
   console.log(process.env.NEXT_PUBLIC_MICROCMS_API_KEY);
   console.log(process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN);
   try {
-    // MicroCMSからコンテンツを取得
     const data = await client.getList<Item>({
       endpoint: "items",
     });
-    // 成功時のレスポンス
+
     res.status(200).json(data);
   } catch (error) {
-    // エラーハンドリング
     console.error("Failed to fetch data:", error);
     res.status(500).json({ error: "Failed to fetch data" });
   }
