d650a1103e8fc8db0b02db88601e48848ff29fde
sncktm <sncktm01@gmail.com>
2024-10-03 23:35:20 +0900

    変数修正

--- src/pages/api/getContent.ts
+++ src/pages/api/getContent.ts
@@ -1,20 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createClient } from "microcms-js-sdk";
 import { Item } from "@/libs/microcms";
-if (!process.env.NEXT_PUBLIC_MICROCMS_API_KEY) {
+if (!process.env.MICROCMS_API_KEY) {
   throw new Error("MICROCMS_SERVICE_DOMAIN is required");
 }
-if (!process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN) {
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
   throw new Error("MICROCMS_API_KEY is required");
 }
 const client = createClient({
-  serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN,
-  apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
 });
 
 export default async function GET(req: NextApiRequest, res: NextApiResponse) {
-  console.log(process.env.NEXT_PUBLIC_MICROCMS_API_KEY);
-  console.log(process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN);
+  console.log(process.env.MICROCMS_API_KEY);
+  console.log(process.env.MICROCMS_SERVICE_DOMAIN);
   try {
     const data = await client.getList<Item>({
       endpoint: "items",
